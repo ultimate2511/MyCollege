7a2e1e2e1824a9dcaa0ecdce58e3d1d2ab0e71bc
anish <anishraj.ar21@gmail.com>
2024-07-13 00:50:59 +0530

    admin side link is added to icon for home page

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.scss';
 import logo from './logo.webp';  // Assuming you have a logo image in the src directory
 import searchicon from './search.png';
 import { FaUser, FaSignInAlt } from 'react-icons/fa';  // Importing icons for user profile and login
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,Link } from 'react-router-dom';
 import UserContext from '../../Context/UserContext';
 
 const Navbar = () => {
@@ -29,10 +29,11 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-logo">
+     <Link to='/'  > <div className="navbar-logo">
         <img src={logo} alt="Logo" />
-        <span className="navbar-name">MyCollege <br />Admin</span>
+       <span className="navbar-name" style ={{color:'white'}}>MyCollege <br />Admin</span>
       </div>
+      </Link>
       <div className="navbar-search">
         <img src={searchicon} alt="Search Icon" />
         <input type="text" placeholder="Search Colleges..." />
